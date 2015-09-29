3f9526ffdd064aefb4acca5beb6ceda823b5c5cb
oalba <odeialba@yahoo.es>
2015-09-29 16:19:44 +0100

    totala begiratzen

--- creating.php
+++ creating.php
@@ -32,6 +32,11 @@ $objPHPExcel->setActiveSheetIndex(0)
             ->mergeCells('B12:E12')
             ->mergeCells('F44:G44');
 
+for ($i=13; $i<=41; $i++){
+    $objPHPExcel->setActiveSheetIndex(0)
+                ->mergeCells('B'.$i.':E'.$i);
+}
+
 
 $borderArray = array(
   'borders' => array(
@@ -66,6 +71,7 @@ $worksheet->getRowDimension(46)->setRowHeight(45);
 
 $worksheet->getStyle('A1:A7')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
 $worksheet->getStyle('A12:G12')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+$worksheet->getStyle('F44')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 
 $worksheet->getStyle('A1')->getFont()->setName('Britannic Bold')->setSize(13)->setBold(true);
 $worksheet->getStyle('A3')->getFont()->setName('Lucida Calligraphy')->setSize(10);
@@ -80,6 +86,9 @@ $worksheet->getStyle('A46')->getFont()->setName('Arial')->setSize(6);
 $worksheet->getStyle('A46')->getAlignment()->setWrapText(true);
 
 $worksheet->getStyle('F13:G41')->getNumberFormat()->setFormatCode('0.00€');
+$worksheet->getStyle('A44')->getNumberFormat()->setFormatCode('0.00€');
+$worksheet->getStyle('C44')->getNumberFormat()->setFormatCode('0.00€');
+$worksheet->getStyle('F44')->getNumberFormat()->setFormatCode('0.00€');
 
 // Add some data
 $objPHPExcel->setActiveSheetIndex(0)
@@ -99,16 +108,25 @@ $objPHPExcel->setActiveSheetIndex(0)
             ->setCellValue('F12', 'Precio')
             ->setCellValue('G12', 'Importe')
             ->setCellValue('A13', '3')
-            ->setCellValue('F13', '2.52');
+            ->setCellValue('F13', '2.52')
+            ->setCellValue('A14', '7')
+            ->setCellValue('F14', '4.63');
 
             for ($i=13; $i<=41; $i++){
-                $objPHPExcel->setActiveSheetIndex(0)->setCellValue('G'.$i, '=A'.$i.'*F'.$i);
+                $vaca = $worksheet->getCell('A'.$i)->getValue();
+                $vacf = $worksheet->getCell('F'.$i)->getValue();
+                if (($vaca!=0||$vaca!="")||($vacf!=0||$vacf!="")){
+                    $objPHPExcel->setActiveSheetIndex(0)->setCellValue('G'.$i, '=A'.$i.'*F'.$i);
+                }
             }
 
 $objPHPExcel->setActiveSheetIndex(0)
             ->setCellValue('A43', 'Subtotal')
+            ->setCellValue('A44', '=SUM(G13:G41)')
             ->setCellValue('C43', 'IVA 21%')
+            ->setCellValue('C44', '=A44*21%')
             ->setCellValue('E44', 'Total:')
+            ->setCellValue('F44', '=A44+C44')
 
             ->setCellValue('A46', 'De conformidad con la Ley Orgánica de Protección de Datos de Carácter Personal 15/1999, le recordamos que sus datos han sido incorporados en un fichero de datos de carácter personal del que es titular ******* **** *******, debidamente registrado ante la AEPD y cuya finalidad es de gestión de datos de clientes para tareas contable, fiscal y administrativas, Así mismo, le informamos que sus datos podrán ser cedidos, siempre protegiendo los datos adecuadamente, a: administración tributaria y bancos, cajas de ahorros y cajas rurales. Puede ejercitar sus derechos de Acceso, Rectificación, Cancelación y Oposición en ******* ** - *****, ******* (*********) o enviando un correo electrónico a *****.****@hotmail.com.');
 
