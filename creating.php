63bf216f19065fb07a57e98922eb93937af99b12
oalba <odeialba@yahoo.es>
2015-09-29 15:51:28 +0100

    totala begiratzen

--- creating.php
+++ creating.php
@@ -13,6 +13,8 @@ require_once dirname(__FILE__) . '/PHPExcel/Classes/PHPExcel.php';
 // Create new PHPExcel object
 $objPHPExcel = new PHPExcel();
 
+$worksheet = $objPHPExcel->getActiveSheet();
+
 $objPHPExcel->setActiveSheetIndex(0)
             ->mergeCells('A46:G46')
             ->mergeCells('A1:C2')
@@ -27,7 +29,8 @@ $objPHPExcel->setActiveSheetIndex(0)
             ->mergeCells('E5:G5')
             ->mergeCells('E6:G6')
             ->mergeCells('F10:G10')
-            ->mergeCells('B12:E12');
+            ->mergeCells('B12:E12')
+            ->mergeCells('F44:G44');
 
 
 $borderArray = array(
@@ -38,43 +41,45 @@ $borderArray = array(
   )
 );
 
-$objPHPExcel->getActiveSheet()->getStyle('E2:G6')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('A10:B10')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('C10:D10')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('E10:G10')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('A12')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('B12:E12')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('F12')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('G12')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('A13:A41')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('B13:E41')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('F13:F41')->applyFromArray($borderArray);
-$objPHPExcel->getActiveSheet()->getStyle('G13:G41')->applyFromArray($borderArray);
+$worksheet->getStyle('E2:G6')->applyFromArray($borderArray);
+$worksheet->getStyle('A10:B10')->applyFromArray($borderArray);
+$worksheet->getStyle('C10:D10')->applyFromArray($borderArray);
+$worksheet->getStyle('E10:G10')->applyFromArray($borderArray);
+$worksheet->getStyle('A12')->applyFromArray($borderArray);
+$worksheet->getStyle('B12:E12')->applyFromArray($borderArray);
+$worksheet->getStyle('F12')->applyFromArray($borderArray);
+$worksheet->getStyle('G12')->applyFromArray($borderArray);
+$worksheet->getStyle('A13:A41')->applyFromArray($borderArray);
+$worksheet->getStyle('B13:E41')->applyFromArray($borderArray);
+$worksheet->getStyle('F13:F41')->applyFromArray($borderArray);
+$worksheet->getStyle('G13:G41')->applyFromArray($borderArray);
 
 unset($borderArray);
 
-$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(11);
-$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(13);
-$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(11);
-$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(13);
-$objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(13);
+$worksheet->getColumnDimension('A')->setWidth(11);
+$worksheet->getColumnDimension('B')->setWidth(13);
+$worksheet->getColumnDimension('C')->setWidth(11);
+$worksheet->getColumnDimension('D')->setWidth(13);
+$worksheet->getColumnDimension('E')->setWidth(13);
 
-$objPHPExcel->getActiveSheet()->getRowDimension(46)->setRowHeight(45);
+$worksheet->getRowDimension(46)->setRowHeight(45);
 
-$objPHPExcel->getActiveSheet()->getStyle('A1:A7')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-$objPHPExcel->getActiveSheet()->getStyle('A12:G12')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+$worksheet->getStyle('A1:A7')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+$worksheet->getStyle('A12:G12')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER)->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
 
-$objPHPExcel->getActiveSheet()->getStyle('A1')->getFont()->setName('Britannic Bold')->setSize(13)->setBold(true);
-$objPHPExcel->getActiveSheet()->getStyle('A3')->getFont()->setName('Lucida Calligraphy')->setSize(10);
-$objPHPExcel->getActiveSheet()->getStyle('A4:A7')->getFont()->setName('Palatino Linotype')->setSize(10);
-$objPHPExcel->getActiveSheet()->getStyle('A12:G12')->getFont()->setName('Century')->setSize(10);
+$worksheet->getStyle('A1')->getFont()->setName('Britannic Bold')->setSize(13)->setBold(true);
+$worksheet->getStyle('A3')->getFont()->setName('Lucida Calligraphy')->setSize(10);
+$worksheet->getStyle('A4:A7')->getFont()->setName('Palatino Linotype')->setSize(10);
+$worksheet->getStyle('A12:G12')->getFont()->setName('Century')->setSize(10);
 
-$objPHPExcel->getActiveSheet()->getStyle('A10')->getFont()->setName('Centaur')->setSize(10)->setBold(true)->setItalic(true);
-$objPHPExcel->getActiveSheet()->getStyle('C10')->getFont()->setName('Centaur')->setSize(10)->setBold(true)->setItalic(true);
-$objPHPExcel->getActiveSheet()->getStyle('E10')->getFont()->setName('Centaur')->setSize(10)->setBold(true)->setItalic(true);
+$worksheet->getStyle('A10')->getFont()->setName('Centaur')->setSize(10)->setBold(true)->setItalic(true);
+$worksheet->getStyle('C10')->getFont()->setName('Centaur')->setSize(10)->setBold(true)->setItalic(true);
+$worksheet->getStyle('E10')->getFont()->setName('Centaur')->setSize(10)->setBold(true)->setItalic(true);
 
-$objPHPExcel->getActiveSheet()->getStyle('A46')->getFont()->setName('Arial')->setSize(6); 
-$objPHPExcel->getActiveSheet()->getStyle('A46')->getAlignment()->setWrapText(true);
+$worksheet->getStyle('A46')->getFont()->setName('Arial')->setSize(6); 
+$worksheet->getStyle('A46')->getAlignment()->setWrapText(true);
+
+$worksheet->getStyle('F13:G41')->getNumberFormat()->setFormatCode('0.00€');
 
 // Add some data
 $objPHPExcel->setActiveSheetIndex(0)
@@ -93,7 +98,14 @@ $objPHPExcel->setActiveSheetIndex(0)
             ->setCellValue('B12', 'Concepto')
             ->setCellValue('F12', 'Precio')
             ->setCellValue('G12', 'Importe')
+            ->setCellValue('A13', '3')
+            ->setCellValue('F13', '2.52');
 
+            for ($i=13; $i<=41; $i++){
+                $objPHPExcel->setActiveSheetIndex(0)->setCellValue('G'.$i, '=A'.$i.'*F'.$i);
+            }
+
+$objPHPExcel->setActiveSheetIndex(0)
             ->setCellValue('A43', 'Subtotal')
             ->setCellValue('C43', 'IVA 21%')
             ->setCellValue('E44', 'Total:')
@@ -102,7 +114,7 @@ $objPHPExcel->setActiveSheetIndex(0)
 
 
 // Rename worksheet
-$objPHPExcel->getActiveSheet()->setTitle('Factura');
+$worksheet->setTitle('Factura');
 
 // Set active sheet index to the first sheet, so Excel opens this as the first sheet
 $objPHPExcel->setActiveSheetIndex(0);
@@ -112,7 +124,7 @@ echo date('H:i:s') , " Write to Excel2007 format" , EOL;
 $callStartTime = microtime(true);
 
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+$objWriter->save('Factura.xlsx');
 $callEndTime = microtime(true);
 $callTime = $callEndTime - $callStartTime;
 
